28394d8a3601408130498488cf69480f4f9c218f
Mark Brouch <mbrouch@walmartlabs.com>
2017-06-19 20:46:16 -0500

    🔧 Configure install script to not run if root package

--- scripts/install.js
+++ scripts/install.js
@@ -4,28 +4,34 @@
 const fs = require("fs");
 const path = require("path");
 
-const rootDir = __dirname.split("/node_modules")[0];
+let rootDir = __dirname.split("/node_modules", 1)[0] || path.resolve(".");
+if (rootDir === __dirname) {
+  rootDir = path.resolve(".");
+}
 
-const pkg = require(path.resolve(rootDir, "package.json"));
+// Do not run install script if this package is the root
+if (rootDir !== path.resolve(".")) {
+  const pkg = require(path.resolve(rootDir, "package.json"));
 
-fs.writeFileSync(
-  path.resolve(rootDir, "package.json"),
-  JSON.stringify(
-    Object.assign(pkg, {
-      scripts: Object.assign(
-        {
-          format:
-            "prettier --print-width 100 --write *.{js,jsx} `find . -type d -d 1 -exec echo '{}/**/*.{js,jsx}' \\; | egrep -v '(/node_modules/|/dist/|/coverage/)'`",
-          precommit: "lint-staged"
-        },
-        pkg.scripts
-      ),
-      "lint-staged": Object.assign(
-        {
-          "**/*.{js,jsx}": ["prettier --print-width 100 --write", "git add"]
-        },
-        pkg["lint-staged"]
-      )
-    })
-  )
-);
+  fs.writeFileSync(
+    path.resolve(rootDir, "package.json"),
+    JSON.stringify(
+      Object.assign(pkg, {
+        scripts: Object.assign(
+          {
+            format:
+              "prettier --print-width 100 --write *.{js,jsx} `find . -type d -d 1 -exec echo '{}/**/*.{js,jsx}' \\; | egrep -v '(/node_modules/|/dist/|/coverage/)'`",
+            precommit: "lint-staged"
+          },
+          pkg.scripts
+        ),
+        "lint-staged": Object.assign(
+          {
+            "**/*.{js,jsx}": ["prettier --print-width 100 --write", "git add"]
+          },
+          pkg["lint-staged"]
+        )
+      })
+    )
+  );
+}
